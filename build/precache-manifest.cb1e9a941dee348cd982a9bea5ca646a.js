e38293892cf660338d8fb97ffb77327911feab06
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2019-01-02 17:48:15 +0700

    build frontend

---
+++ build/precache-manifest.cb1e9a941dee348cd982a9bea5ca646a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62b6d93696e3274ececd",
+    "url": "/static/js/main.62b6d936.chunk.js"
+  },
+  {
+    "revision": "ffde7bc85bc34fcd0ac9",
+    "url": "/static/js/1.ffde7bc8.chunk.js"
+  },
+  {
+    "revision": "62b6d93696e3274ececd",
+    "url": "/static/css/main.bae9feac.chunk.css"
+  },
+  {
+    "revision": "ffde7bc85bc34fcd0ac9",
+    "url": "/static/css/1.16f0f8c7.chunk.css"
+  },
+  {
+    "revision": "f601edaee3c480de62511d28ce5921ba",
+    "url": "/index.html"
+  }
+];
